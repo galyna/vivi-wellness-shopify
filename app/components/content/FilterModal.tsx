adf5efe330cb00ecc6baa37c7c1b4b372a66baa9
galyna <galynavistovska@gmail.com>
2025-07-07 21:30:43 +0300

    fix: update filter modal import

--- app/components/content/FilterModal.tsx
+++ app/components/content/FilterModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useFilters } from "../../hooks/product/useFilters";
+import { useFilters } from "../../hooks/filters/useFilters";
 
 interface FilterSettings {
   categories?: string[];
