c190b3dd38c5e8043c7136aab00d1c8a02900cdd
galyna <galynavistovska@gmail.com>
2025-07-09 17:38:38 +0300

    feat: add sanity-cli.d.ts

---
+++ types/sanity-cli.d.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare module 'sanity/cli' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export function defineCliConfig(config: any): any;
+} 
